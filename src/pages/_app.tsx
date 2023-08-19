101647df4a33684810c211f5d9b23fb968558170
Rahmat Sulistio <rr7465457@gmail.com>
2023-08-20 02:12:09 +0700

    :heavy_plus_sign: Add mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,44 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
-import { api } from "~/utils/api";
-import "~/styles/globals.css";
+import { MantineProvider } from '@mantine/core'
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import { type AppType } from 'next/app'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+
+import { api } from '~/utils/api'
+import '~/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
-};
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
 
-export default api.withTRPC(MyApp);
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+          primaryColor: 'teal',
+          fontFamily: inter.style.fontFamily,
+        }}
+      >
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </MantineProvider>
+    </>
+  )
+}
+
+export default api.withTRPC(MyApp)
